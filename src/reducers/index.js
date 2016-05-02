2db77ef807a41a4da9fd5b14dbcbdaa218fdc6a0
Darryl Blake <darryl@darrylblake.com>
2016-05-02 17:54:33 +1200

    Working through tutorial.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+// was adding the reducer....
+import ActiveBookReducer from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
